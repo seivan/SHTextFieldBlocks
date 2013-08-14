1202f9e3bfdbaba00832c5867f6718c4ec8cdef2
Seivan Heidari <seivan@kth.se>
2013-08-15 00:02:43 +0200

    Update sample.

--- Example/Example/SHFirstViewController.m
+++ Example/Example/SHFirstViewController.m
@@ -16,8 +16,13 @@
 
 @implementation SHFirstViewController
 
--(void)viewDidAppear:(BOOL)animated; {
-  [self performSegueWithIdentifier:@"second" sender:nil];
+-(void)viewDidLoad; {
+  [super viewDidLoad];
+  double delayInSeconds = 2.0;
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      [self performSegueWithIdentifier:@"second" sender:nil];
+  });
 }
 
 
